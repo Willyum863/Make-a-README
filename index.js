785be8e8d54d23cb5ea5dc2623a3afa1fd6796d2
William Combee <william.combee@yahoo.com>
2022-09-13 13:07:31 -0500

    starting questions

--- index.js
+++ index.js
@@ -1,7 +1,37 @@
 // TODO: Include packages needed for this application
-
+const fs=require("fs");
+const path=require("path")
+const inquirer=require("inquirer")
+const generateMarkdown=require("./utils/generateMarkdown")
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        name: "github",
+        message: "what is your github username?"
+    },
+    {
+        type: "input",
+        name: "github",
+        message: "what is your github username?"
+    },
+    {
+        type: "input",
+        name: "github",
+        message: "what is your github username?"
+    },
+    {
+        type: "input",
+        name: "github",
+        message: "what is your github username?"
+    },
+    {
+        type: "list",
+        name: "license",
+        message: "what kind of license shoud your project have?",
+        choices: ["MIT","None"]
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
