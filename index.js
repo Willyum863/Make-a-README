97bd677f5f9241a045d887612e5d97a496a8fba8
William Combee <william.combee@yahoo.com>
2022-09-14 20:19:32 -0500

    adding questions

--- index.js
+++ index.js
@@ -71,8 +71,10 @@ function writeToFile(fileName, data) {
 // TODO: Create a function to initialize app
 function init() {
     inquirer.prompt(questions).then((data) => {
+        console.log(data);
         const readMeMaker = generateMarkdown({...data});
-        writeToFile("README.md",readMeMaker)
+        console.log(readMeMaker);
+        writeToFile("README.md",readMeMaker);
     });
 }
     
